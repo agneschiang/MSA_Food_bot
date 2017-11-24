0d69a0035e0ade4625fedd6a7d88d459043f58f1
Agnes Chiang <agnesninja@gmail.com>
2017-11-24 14:25:07 +1300

    Add project files and folders

--- controller/QnAMaker.js
+++ controller/QnAMaker.js
@@ -1,7 +1,7 @@
 var rest = require('../API/Restclient');
 
 exports.talkToQnA = function postQnAResults(session, question){
-    var url = 'https://westus.api.cognitive.microsoft.com/qnamaker/v2.0/knowledgebases/[REFER TO IMAGE ABOVE]/generateAnswer';
+    var url = 'https://westus.api.cognitive.microsoft.com/qnamaker/v2.0/knowledgebases/a0051e20a99444c58bc60e0bbabf79fe/generateAnswer';
     rest.postQnAResults(url, session, question, handleQnA)
 };
 
